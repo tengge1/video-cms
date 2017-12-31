a7ac222abfd04572dd9f68c9a8667a875eabbfc9
liteng <930372551@qq.com>
2017-12-31 21:56:18 +0800

    serving file

--- index.py
+++ index.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello,world!'
+	return render_template('index.html')
+
+@app.route('/login', methods = [ 'GET', 'POST' ])
+def login():
+	if request.method == 'POST':
+		pass
+	else:
+		pass
+
+@app.route('/list/<int:type_id>')
+def list(type_id):
+	pass
+
+@app.route('/video/<int:video_id>')
+def video(video_id):
+	pass
+
--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 set FLASK_APP=index.py
-flask run
+set FLASK_DEBUG=1
+flask run --host=0.0.0.0
