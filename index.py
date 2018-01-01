74f812b23c5ef7c53cac94e85644876f47d8da4a
liteng <930372551@qq.com>
2018-01-01 14:37:24 +0800

    add login page

--- index.py
+++ index.py
@@ -1,17 +1,37 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, session, json
+from base.sqlhelper import SqlHelper
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'video-cms'
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	if session.get('username') == None:
+		return render_template('login.html')
+	else:
+		return render_template('index.html')
 
 @app.route('/login', methods = [ 'GET', 'POST' ])
 def login():
 	if request.method == 'POST':
-		pass
+		username = request.values.get('username')
+		password = request.values.get('password')
+		helper = SqlHelper()
+		sql = 'select * from user where username=%s and password=%s'
+		result = helper.fetchone(sql, (username, password));
+		if(result == None):
+			return json.dumps({
+				'success': 'false',
+				'msg': 'Username or password is wrong!'
+				});
+		else:
+			session['username'] = result['username']
+			return json.dumps({
+				'success': 'true',
+				'msg': 'Login succeed!'
+				});
 	else:
-		pass
+		return render_template('login.html')
 
 @app.route('/list/<int:type_id>')
 def list(type_id):
