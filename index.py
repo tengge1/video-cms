c822e4f579a818ec4baa24de9112ff8a7c3e5359
liteng <930372551@qq.com>
2018-01-04 21:29:03 +0800

    change code

--- index.py
+++ index.py
@@ -5,52 +5,53 @@ from base.sqlhelper import SqlHelper
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'video-cms'
 
+def init():
+	g.website_name = website_name
+	g.username = session.get('username')
+	if g.username is not None:
+		helper = SqlHelper()
+		sql = "select * from category"
+		g.categories = helper.fetchall(sql)
+
 @app.route('/')
 @app.route('/index')
 @app.route('/index/<int:category_id>')
 def index(category_id = None):
-	g.website_name = website_name
-	if session.get('username') == None:
+	init()
+	if g.username == None:
 		return render_template('login.html')
 	else:
-		sql = "select * from category"
+		g.category_id = category_id
 		helper = SqlHelper()
-		categories = helper.fetchall(sql)
-
-		videos = None
 		if category_id is None:
 			sql = "select * from video"
-			videos = helper.fetchall(sql)
+			g.videos = helper.fetchall(sql)
 		else:
 			sql = "select * from video where category_id=%s"
-			videos = helper.fetchall(sql, (category_id,))
+			g.videos = helper.fetchall(sql, (category_id,))
 
-		return render_template('index.html', data = {
-			'categories': categories,
-			'current_id': category_id,
-			'videos': videos
-			});
+		return render_template('index.html')
 
 @app.route('/login', methods = [ 'GET', 'POST' ])
 def login():
-	g.website_name = website_name
+	init()
 	if request.method == 'POST':
 		username = request.values.get('username')
 		password = request.values.get('password')
 		helper = SqlHelper()
 		sql = 'select * from user where username=%s and password=%s'
 		result = helper.fetchone(sql, (username, password));
-		if(result == None):
+		if result is None:
 			return json.dumps({
 				'success': 'false',
 				'msg': 'Username or password is wrong!'
-				});
+				})
 		else:
 			session['username'] = result['username']
 			return json.dumps({
 				'success': 'true',
 				'msg': 'Login success!'
-				});
+				})
 	else:
 		return render_template('login.html')
 
@@ -60,84 +61,76 @@ def logout():
 	return json.dumps({
 		'success': 'true',
 		'msg': 'Logout success!'
-		});
+		})
 
-@app.route('/video/<int:video_id>')
-def video(video_id):
+@app.route('/video/<int:id>')
+def video(id):
+	init()
+	helper = SqlHelper()
 	sql = "select * from video where id=%s"
-	helper = SqlHelper()
-	video = helper.fetchone(sql, (video_id,))
-
-	sql = "select * from category"
-	helper = SqlHelper()
-	categories = helper.fetchall(sql)
-
-	g.website_name = website_name
-	return render_template('video.html', data = {
-		'video': video,
-		'categories': categories
-		});
+	g.video = helper.fetchone(sql, (id,))
+	return render_template('video.html')
 
 @app.route('/manage/category')
 def category_manage():
-	pageSize = request.values.get('pageSize')
-	pageNum = request.values.get('pageNum')
+	init()
+	g.pageSize = request.values.get('pageSize')
+	g.pageNum = request.values.get('pageNum')
 
-	if pageSize is None:
-		pageSize = 10
-	if pageNum is None:
-		pageNum = 1
+	if g.pageSize is None:
+		g.pageSize = 10
+	if g.pageNum is None:
+		g.pageNum = 1
 	
 	helper = SqlHelper()
-	sql = "select * from category"
-	categories = helper.fetchall(sql)
-
 	sql = "select count(1) from category"
-	total = helper.fetchone(sql)
+	g.total = helper.fetchone(sql)
 
 	sql = "select * from category order by id desc limit %s,%s "
-	rows = helper.fetchall(sql, (pageSize * (pageNum - 1), pageSize))
-
-	g.website_name = website_name
-	g.categories = categories
-	g.pageSize = pageSize
-	g.pageNum = pageNum
-	g.total = total
-	g.rows = rows
+	g.rows = helper.fetchall(sql, (g.pageSize * (g.pageNum - 1), g.pageSize))
+	
 	return render_template('manage/category.html');
+
+@app.route('/manage/category/add', methods = [ 'GET', 'POST' ])
+def category_add():
+	init()
+	helper = SqlHelper()
+	if request.method == 'POST':
+		name = request.values.get('name')
+		sql = "insert into category (name) values (%s)"
+		helper.execute(sql, (name,))
+		return json.dumps({
+			'success': 'true',
+			'msg': 'Save success!'
+			})
+	else:
+		return render_template('manage/category_add.html');
+
+@app.route('/manage/category/delete/<int:id>', methods = ['POST'])
+def category_delete(id):
+	init()
+	helper = SqlHelper()
+	sql = "delete from category where id=%s"
+	helper.execute(sql, (id,))
+	return json.dumps({
+		'success': 'true',
+		'msg': 'Delete success!'
+		})
 
 @app.route('/manage/video')
 def video_manage():
-	sql = "select * from category"
-	helper = SqlHelper()
-	categories = helper.fetchall(sql)
-
-	g.website_name = website_name
-	return render_template('manage/video.html', data = {
-		'categories': categories,
-		});
+	init()
+	return render_template('manage/video.html')
 
 @app.route('/manage/user')
 def user_manage():
-	sql = "select * from category"
-	helper = SqlHelper()
-	categories = helper.fetchall(sql)
-
-	g.website_name = website_name
-	return render_template('manage/user.html', data = {
-		'categories': categories,
-		});
+	init()
+	return render_template('manage/user.html')
 
 @app.route('/manage/setting')
 def setting():
-	sql = "select * from category"
-	helper = SqlHelper()
-	categories = helper.fetchall(sql)
-
-	g.website_name = website_name
-	return render_template('manage/setting.html', data = {
-		'categories': categories,
-		});
+	init()
+	return render_template('manage/setting.html')
 
 @app.route('/upload/<path:path>')
 def send_upload(path):
