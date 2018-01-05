6ff7d6852458e23c1911392a9ff65e2a60da89e7
liteng <930372551@qq.com>
2018-01-05 23:00:43 +0800

    user manage

--- index.py
+++ index.py
@@ -88,12 +88,12 @@ def category_manage():
 	helper = SqlHelper()
 	sql = "select count(1) total from category"
 	g.total = helper.fetchone(sql)['total']
-	g.totalPage = g.total / g.pageSize if g.total % g.pageSize == 0 else g.total // g.pageSize + 1
+	g.totalPage = int(g.total / g.pageSize) if g.total % g.pageSize == 0 else g.total // g.pageSize + 1
 
 	sql = "select * from category order by id desc limit %s,%s "
 	g.rows = helper.fetchall(sql, (g.pageSize * (g.pageNum - 1), g.pageSize))
 	
-	g.category_id = 'category_manage'
+	g.category_id = 'system'
 	return render_template('manage/category.html');
 
 @app.route('/manage/category/edit', methods = [ 'GET','POST' ])
@@ -121,7 +121,7 @@ def category_edit():
 			sql = "select name from category where id=%s"
 			category = helper.fetchone(sql, (id,))
 			g.name = category['name']
-		g.category_id = 'category_manage'
+		g.category_id = 'system'
 		return render_template('manage/category_edit.html');
 
 @app.route('/manage/category/delete/<int:id>', methods = ['POST'])
@@ -143,7 +143,72 @@ def video_manage():
 @app.route('/manage/user')
 def user_manage():
 	init()
-	return render_template('manage/user.html')
+	g.pageSize = request.values.get('pageSize')
+	g.pageNum = request.values.get('pageNum')
+
+	if g.pageSize is None:
+		g.pageSize = 10
+	if g.pageNum is None:
+		g.pageNum = 1
+
+	g.pageNum = int(g.pageNum)
+	g.pageSize = int(g.pageSize)
+	
+	helper = SqlHelper()
+	sql = "select count(1) total from user"
+	g.total = helper.fetchone(sql)['total']
+	g.totalPage = int(g.total / g.pageSize) if g.total % g.pageSize == 0 else g.total // g.pageSize + 1
+
+	sql = "select * from user order by id desc limit %s,%s "
+	g.rows = helper.fetchall(sql, (g.pageSize * (g.pageNum - 1), g.pageSize))
+	
+	g.category_id = 'system'
+	return render_template('manage/user.html');
+
+@app.route('/manage/user/edit', methods = [ 'GET','POST' ])
+def user_edit():
+	init()
+	id = request.values.get('id')
+	id = 0 if id is None else int(id)
+	helper = SqlHelper()
+	if request.method == 'POST':
+		username = request.values.get('username')
+		password = request.values.get('password')
+		name = request.values.get('name')
+		if id == 0:
+			sql = "insert into user (username,password,name) values (%s,%s,%s)"
+			helper.execute(sql, (username,password,name))
+		else:
+			sql = "update user set username=%s,password=%s,name=%s where id=%s"
+			helper.execute(sql, (username, password, name, id))
+		return json.dumps({
+			'success': 'true',
+			'msg': 'Save success!'
+			})
+	else:
+		g.id = id
+		g.username = ''
+		g.password = ''
+		g.name = ''
+		if g.id > 0:
+			sql = "select username,password,name from user where id=%s"
+			category = helper.fetchone(sql, (id,))
+			g.username = category['username']
+			g.password = category['password']
+			g.name = category['name']
+		g.category_id = 'system'
+		return render_template('manage/user_edit.html');
+
+@app.route('/manage/user/delete/<int:id>', methods = ['POST'])
+def user_delete(id):
+	init()
+	helper = SqlHelper()
+	sql = "delete from user where id=%s"
+	helper.execute(sql, (id,))
+	return json.dumps({
+		'success': 'true',
+		'msg': 'Delete success!'
+		})
 
 @app.route('/manage/setting')
 def setting():
